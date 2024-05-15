09d948a0dbae0e051c16a9adb30b4f96a15cc7e5
Ibrahim Memon <ibrahim@webhr.email>
2024-05-15 17:28:22 +0500

    tawk update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,23 @@
+"use client";
+import React, { useEffect } from "react";
 import { Social_Icons } from "@/constant";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 
 const Footer: React.FC<{}> = () => {
+  useEffect(() => {
+    var Tawk_LoadStart = new Date();
+    (function () {
+      var s1 = document.createElement("script"),
+        s0 = document.getElementsByTagName("script")[0];
+      s1.async = true;
+      s1.src = process.env.NEXT_PUBLIC_TAWK_TO_ID;
+      s1.charset = "UTF-8";
+      s1.setAttribute("crossorigin", "*");
+      s0.parentNode.insertBefore(s1, s0);
+    })();
+  }, []);
+
   return (
     <div className=" container mx-auto 2xl pt-[50px] pb-8 ">
       <div className="pb-10 justify-center flex ">
@@ -20,8 +34,9 @@ const Footer: React.FC<{}> = () => {
                 src={social.image}
                 height={30}
                 width={30}
-                className="mx-5"
                 alt={social.alt}
+                sizes="100vw"
+                className="mx-5"
               />
             </Link>
           );
