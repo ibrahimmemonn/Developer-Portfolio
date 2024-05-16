8d927496f183ea947900c30ee6ee32444647ca8a
Ibrahim Memon <ibrahim@webhr.email>
2024-05-16 10:14:18 +0500

    updates

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -11,10 +11,10 @@ const Footer: React.FC<{}> = () => {
       var s1 = document.createElement("script"),
         s0 = document.getElementsByTagName("script")[0];
       s1.async = true;
-      s1.src = process.env.NEXT_PUBLIC_TAWK_TO_ID;
+      s1.src = process.env.NEXT_PUBLIC_TAWK_TO_ID ?? "";
       s1.charset = "UTF-8";
       s1.setAttribute("crossorigin", "*");
-      s0.parentNode.insertBefore(s1, s0);
+      s0.parentNode?.insertBefore(s1, s0);
     })();
   }, []);
 
