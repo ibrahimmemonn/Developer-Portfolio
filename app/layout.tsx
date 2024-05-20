ee2d96bc5c22d8f7a3b8d3a6f225f63035f9cf5a
Ibrahim Memon <ibrahim@webhr.email>
2024-05-20 09:59:29 +0500

    vercel dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 import StarsCanvas from "@/components/main/StarsBackground";
 import Navbar from "@/components/Navbar";
 
@@ -42,6 +43,7 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-[#111] overflow-y-scroll overflow-x-hidden`}
       >
+        <Analytics />
         <StarsCanvas />
         <Navbar />
         {children}
